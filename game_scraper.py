51fe9ce01ab4afa36d7a8e5ac13a7f51432a0928
rlundstrom31 <54489162+rlundstrom31@users.noreply.github.com>
2022-06-06 11:25:57 -0500

    created game_scraper, round_scraper to scrape individual games.

---
+++ game_scraper.py
@@ -0,0 +1,64 @@
+# things to calculate
+# number right/wrong
+# coryat / adjusted coryat
+# for wayyy later - category breakdowns.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+max_coryat = 54000
+
+
+class GameScraper:
+
+    def __init__(self, URL):
+        self.URL = URL
+        self.coryat = 0
+        self.first_round_coryat = 0
+        self.second_round_coryat = 0
+        self.possible_score = 0
+        self.adjusted_coryat = 0
+        self.num_correct = 0
+        self.num_wrong = 0
+        self.daily_double = 0
+        self.final = False
+
+    def scrape_game(self, driver):
+        driver.get(self.URL)
+        self.scrape_round(driver, 1)
+        driver.find_element(By.ID, 'round-two-link').click()
+        self.scrape_round(driver, 2)
+        final_right = driver.find_element(By.ID, 'button-final-right')
+        if final_right.get_attribute("class") == 'active':
+            self.final = True
+        print(str(self.coryat) + ' first: ' + str(self.first_round_coryat) + ' second: ' + str(self.second_round_coryat))
+        print(self.daily_double)
+        print(self.final)
+
+    def scrape_round(self, driver, round_number):
+        round_coryat = 0
+        for i in range(1, 7):
+            for j in range(1, 6):
+                clue = driver.find_element(By.ID, 'clue-box-' + str(i) + '-' + str(j)).get_attribute("class")
+                clue = clue[9:]
+                if clue != "clue-nr":
+                    points = 200 * j * round_number
+                    self.possible_score += points
+                    if clue == "clue-right":
+                        round_coryat += points
+                        self.num_correct += 1
+                    elif clue == "clue-wrong":
+                        round_coryat -= points
+                        self.num_wrong += 1
+                    elif (clue == "dd-box clue-right") | (clue == "clue-right dd-box"):
+                        round_coryat += points
+                        self.daily_double += 1
+        self.coryat += round_coryat
+        if round_number == 1:
+            self.first_round_coryat = round_coryat
+        else:
+            self.second_round_coryat = round_coryat
+
--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import os
 import time
 from datetime import time
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+import game_scraper
 import requests
 from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
-
     service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(options = options, service=service)
     email = os.environ['my_email']
     password = os.environ['my_password']
     driver.get('https://www.j-scorer.com/login')
     driver.find_element(By.NAME, 'session[email]').send_keys(email)
     driver.find_element(By.NAME, 'session[password]').send_keys(password)
     driver.find_element(By.NAME, 'commit').click()
-    driver.get('https://j-scorer.com/game?g=2022-02-23')
-    print(driver.find_element(By.ID, 'topic-area-1'))
-    soup = BeautifulSoup(driver.page_source)
-    print(soup.text)
+    game = game_scraper.GameScraper('https://j-scorer.com/game?g=2022-02-24')
+    game.scrape_game(driver)
+
+
+
+    # element = driver.find_element(By.ID, 'topic-area-1')
+    # print(element.get_attribute("value"))
